7f1204c3b136db8683bab4781d71c32f31a8b3c7
Bulent Coskun <bcoskun@conveyhs.com>
2017-07-31 12:42:25 -0400

    Inital Setup

---
+++ PowerShell_Component/MoveFiles.ps1
@@ -0,0 +1,8 @@
+﻿$filelist = Get-Content .\FileListForMove.txt 
+
+$filelist | foreach { 
+    $sourcefile = 'E:\PNCImageRoot\InhouseMailImages\XMLFiles\' + $_ 
+    $destfile = 'E:\PNCImageRoot\InhouseMailImages\backupxml\' + $_
+    mv $sourcefile -Destination $destfile
+    Write-Host "file moved to " $destfile
+}
